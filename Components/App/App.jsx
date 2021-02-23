6ebfa61f3d38622fd48cd4b1fd3ff3292c40453d
Lewis Sneddon <>
2021-02-23 17:55:58 +0000

    set up prettier for formatting

---
+++ Components/App/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div class="App">
+      <div class="header">
+        <h1>Bake</h1>
+        <h1>Buddy</h1>
+      </div>
+      <div class="main-window"></div>
+      <div class="footer">
+        <p>
+          Bake Buddy 2020 by <a href="http://lsneddon.com">Lewis Sneddon</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
